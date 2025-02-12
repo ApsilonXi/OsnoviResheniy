fc1303fdd4e8342d03e461d313d23f0afb038eca
ApsilonXi <volkov.volkova2000@mail.ru>
2025-02-12 12:00:40 +0300

    0

--- lab1/lab1.py
+++ lab1/lab1.py
@@ -34,9 +34,12 @@ def hdmt_schedule(tasks, processors):
 
 # Основная программа
 def main():
-    processors = int(input("N: ")) # количество процессоров
     num_tasks = int(input("M: "))  # количество заданий
-    num_massives = int(input("Кол-во массивов: ")) # количество списков тасков
+    processors = int(input("N: ")) # количество процессоров
+    rand_min = int(input("Min: "))
+    rand_max = int(input("Max: "))
+    '''num_massives = int(input("Кол-во массивов: ")) # количество списков тасков'''
+    num_massives = 50
 
     all_cmp = []
     all_cmp_asc = []
@@ -45,9 +48,19 @@ def main():
     all_hdmt = []
     all_hdmt_asc = []
     all_hdmt_desc = []
+
+    cmp_rand = 0
+    cmp_asc = 0
+    cmp_desc = 0
+
+    hdmt_rand = 0
+    hdmt_asc = 0
+    hdmt_desc = 0
     
     for i in range(num_massives):
-        tasks = random.sample(range(1, 101), num_tasks)
+        tasks = []
+        for i in range(num_tasks):
+            tasks.append(random.randint(rand_min, rand_max))
         tasks_asc = sorted(tasks)
         task_desc = sorted(tasks, reverse=True)
 
@@ -67,10 +80,33 @@ def main():
         all_hdmt_asc.append(max(result_hdmt_asc))
         all_hdmt_desc.append(max(result_hdmt_desc))
 
-    
+        max_val_cmp = min([max(result_cmp), max(result_cmp_asc), max(result_cmp_desc)])
+
+        if max_val_cmp == max(result_cmp_desc):
+            cmp_desc += 1 
+        elif max_val_cmp == max(result_cmp):
+            cmp_rand += 1
+        elif max_val_cmp == max(result_cmp_asc):
+            cmp_asc += 1
+           
+            '''print(tasks_asc, result_cmp_asc)
+            print(task_desc, result_cmp_desc)  '''  
+
+        max_val_hdmt = min([max(result_hdmt), max(result_hdmt_asc), max(result_hdmt_desc)])
+
+        if max_val_hdmt == max(result_hdmt_desc):
+            hdmt_desc += 1
+        elif max_val_hdmt ==  max(result_hdmt):
+            hdmt_rand += 1
+        elif max_val_hdmt == max(result_hdmt_asc):
+            hdmt_asc += 1
+
+
+        
     for i in [all_cmp, all_cmp_asc, all_cmp_desc]:
         aver = sum(i)/len(i)
         i.append(aver)
+
 
     for i in [all_hdmt, all_hdmt_asc, all_hdmt_desc]:
         aver = sum(i)/len(i)
@@ -79,16 +115,16 @@ def main():
     print("----------------------")
 
     print("CMP: ")
-    print("Случайно: ", all_cmp[len(all_cmp)-1])
-    print("По возрастанию: ", all_cmp_asc[len(all_cmp_asc)-1])
-    print("По убыванию: ", all_cmp_desc[len(all_cmp_desc)-1])
+    print("Случайно: ", all_cmp[len(all_cmp)-1], cmp_rand) 
+    print("По возрастанию: ", all_cmp_asc[len(all_cmp_asc)-1], cmp_asc)
+    print("По убыванию: ", all_cmp_desc[len(all_cmp_desc)-1], cmp_desc)
 
     print("----------------------")
 
     print("HDMT")
-    print("Случайно: ", all_hdmt[len(all_hdmt)-1])
-    print("По возрастанию: ", all_hdmt_asc[len(all_hdmt_asc)-1])
-    print("По убыванию: ", all_hdmt_desc[len(all_hdmt_desc)-1])
+    print("Случайно: ", all_hdmt[len(all_hdmt)-1], hdmt_rand)
+    print("По возрастанию: ", all_hdmt_asc[len(all_hdmt_asc)-1], hdmt_asc)
+    print("По убыванию: ", all_hdmt_desc[len(all_hdmt_desc)-1], hdmt_desc)
         
 
 if __name__ == "__main__":
