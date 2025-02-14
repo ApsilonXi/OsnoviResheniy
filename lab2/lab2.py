b6ded3977608684df3ce05b3c8e54efa14fc5daf
ApsilonXi <volkov.volkova2000@mail.ru>
2025-02-14 14:46:28 +0300

    обнова второй лабы

--- lab2/lab2.py
+++ lab2/lab2.py
@@ -1,22 +1,25 @@
 import numpy as np
 import random
 
-def generate_matrix(num_tasks, num_processors, min_val, max_val):
-    # Генерация случайной матрицы с заданным количеством задач и процессоров
-    m = np.random.randint(min_val, max_val + 1, size=(num_tasks, num_processors))
-    return m
+def schedule_with_arbitrary_load(T, N, M):
+    load = [0 for i in range(N)]
+    for i in T:
+        min_el = min(i[:N])
+        min_index = i.index(min_el)
+        if load[min_index] == 0:
+            load[min_index] += min_el
+        elif load[min_index] > 0:
+            load_min_index = load.index(min(load))
+            load[load_min_index] += i[load_min_index]
+    return load, max(load)
 
-def schedule_with_arbitrary_load(matrix):
-    # Массив для хранения нагрузки на процессоры
-    load = np.zeros(matrix.shape[1])
-
-    # Шаг 2 и Шаг 3: Пробегаем по строкам и выбираем минимальный элемент
-    for row in matrix:
-        min_index = np.argmin(load + row)
-        load[min_index] += row[min_index]
-
-    # Возвращаем итоговую максимальную загрузку
-    return load, np.max(load)
+def sorting(N, T, sort_arg = -1):
+    if sort_arg == -1:
+        return T
+    elif sort_arg == 0:
+        return sorted(T, key=lambda x: x[N])
+    elif sort_arg == 1:
+        return sorted(T, key=lambda x: x[N], reverse=True)
 
 # Основная программа
 def main():
@@ -27,50 +30,45 @@ def main():
     '''num_massives = int(input("Кол-во массивов: ")) # количество списков тасков'''
     num_massives = 100
 
-    all_cmp = []
-    all_cmp_asc = []
-    all_cmp_desc = []
+    all_schedule = []
+    all_schedule_asc = []
+    all_schedule_desc = []
 
-    cmp_rand = 0
-    cmp_asc = 0
-    cmp_desc = 0
+    schedule_rand = 0
+    schedule_asc = 0
+    schedule_desc = 0
     
     for i in range(num_massives):
-        tasks = generate_matrix(num_tasks, processors, rand_min, rand_max)
-        tasks_asc = []
-        for i in range(len(tasks)):
-            tasks_asc.append(sorted(tasks[i]))
-        tasks_desc = []
-        for i in range(len(tasks)):
-            tasks_desc.append(sorted(tasks[i], reverse=True))
+        tasks = np.random.randint(rand_min, rand_max, (num_tasks, processors)).tolist()
+        tasks_asc = sorting(processors-1, tasks, 0)
+        tasks_desc = sorting(processors-1, tasks, 1)
 
-        result_cmp, max_1 = schedule_with_arbitrary_load(tasks)
-        result_cmp_asc, max_2= schedule_with_arbitrary_load(np.array(tasks_asc))
-        result_cmp_desc, max_3= schedule_with_arbitrary_load(np.array(tasks_desc))
+        result_schedule, max_rand = schedule_with_arbitrary_load(tasks, processors, num_tasks)
+        result_schedule_asc, max_asc = schedule_with_arbitrary_load(tasks_asc, processors, num_tasks)
+        result_schedule_desc, max_desc = schedule_with_arbitrary_load(tasks_desc, processors, num_tasks)
 
-        all_cmp.append(max_1)
-        all_cmp_asc.append(max_2)
-        all_cmp_desc.append(max_3)
+        all_schedule.append(max_rand)
+        all_schedule_asc.append(max_asc)
+        all_schedule_desc.append(max_desc)
 
-        max_val_cmp = min([max_1, max_2, max_3])
+        max_val_schedule = min([max_rand, max_asc, max_desc])
 
-        if max_val_cmp == max(max_3):
-            cmp_desc += 1 
-        elif max_val_cmp == max(max_1):
-            cmp_rand += 1
-        elif max_val_cmp == max(max_2):
-            cmp_asc += 1
+        if max_val_schedule == max_desc:
+            schedule_desc += 1 
+        elif max_val_schedule == max_rand:
+            schedule_rand += 1
+        elif max_val_schedule == max_asc:
+            schedule_asc += 1
         
-    for i in [all_cmp, all_cmp_asc, all_cmp_desc]:
+    for i in [all_schedule, all_schedule_asc, all_schedule_desc]:
         aver = sum(i)/len(i)
         i.append(aver)
         
     print("-------Lab 1----------")
 
-    print("Случайно: ", all_cmp[len(all_cmp)-1], cmp_rand) 
-    print("По возрастанию: ", all_cmp_asc[len(all_cmp_asc)-1], cmp_asc)
-    print("По убыванию: ", all_cmp_desc[len(all_cmp_desc)-1], cmp_desc)
-
+    print("Случайно: ", all_schedule[len(all_schedule)-1], schedule_rand) 
+    print("По возрастанию: ", all_schedule_asc[len(all_schedule_asc)-1], schedule_asc)
+    print("По убыванию: ", all_schedule_desc[len(all_schedule_desc)-1], schedule_desc)
         
 
 if __name__ == "__main__":
